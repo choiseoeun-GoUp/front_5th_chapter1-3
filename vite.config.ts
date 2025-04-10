1683b6749320f726a1c3434257ba599ce1a5422e
choiseoeun-GoUp <seoeun0321@gmail.com>
2025-04-10 15:40:22 +0900

    chore : 배포

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-
 export default mergeConfig(
   defineConfig({
     base: "", // base 넣기
