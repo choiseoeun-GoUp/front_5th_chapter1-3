0f0ef703ccd48b47e8c675ff25f6237f5e653182
ui-choiseoeun <sechoi@sinsiway.com>
2025-04-10 15:35:54 +0900

    chore : 배포

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+
 export default mergeConfig(
   defineConfig({
+    base: "", // base 넣기
     plugins: [react()],
   }),
   defineTestConfig({
